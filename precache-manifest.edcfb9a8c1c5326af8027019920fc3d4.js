8a5e3ad7cbf2f684d16256e3783adc77b23da2c5
codyswick <47154623+codyswick@users.noreply.github.com>
2019-04-07 19:56:23 -0500

    Updates

---
+++ precache-manifest.edcfb9a8c1c5326af8027019920fc3d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "23bfaf9318907b61b2a9",
+    "url": "/portfolio/static/js/main.afe58b04.chunk.js"
+  },
+  {
+    "revision": "f06b7a7d8f1938c323cc",
+    "url": "/portfolio/static/js/2.20a438c3.chunk.js"
+  },
+  {
+    "revision": "23bfaf9318907b61b2a9",
+    "url": "/portfolio/static/css/main.6ad9e1d5.chunk.css"
+  },
+  {
+    "revision": "812b62cabfe640fd4ad0b97a21bc8fac",
+    "url": "/portfolio/index.html"
+  }
+];
